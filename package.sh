a426ba2bef8d51c8ae3a57cbbf9f28611bdfea1d
mtajmajer <Michael.Tajmajer@Emerson.com>
2017-08-21 11:11:24 -0500

    update helper script

--- package.sh
+++ package.sh
@@ -2,5 +2,6 @@
 cd packages
 tar -zcf react-data-grid.tar.gz react-data-grid/ --exclude react-data-grid/.vscode
 tar -zcf react-data-grid-addons.tar.gz react-data-grid-addons/ --exclude react-data-grid-addons/node_modules --exclude react-data-grid-addons/.vscode
-cp -f *.tar.gz /mnt/c/_Thing_WrkStn_V14Development/XSrc/NpmPack
+#cp -f *.tar.gz /mnt/c/_Thing_WrkStn_V14Development/XSrc/NpmPack
+cp -f *.tar.gz /mnt/d/_CIF_Thing_V14Development/XSrc/NpmPack
 cd ..
